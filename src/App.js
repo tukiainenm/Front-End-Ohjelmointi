2c8201ab1a623d8ccc70f476e7abd6cd6f2e4081
tukiainenm <83721917+tukiainenm@users.noreply.github.com>
2022-04-05 18:53:11 +0300

    TodoList tehty

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Todotable from './components/todoList';
 
 function App() {
+  const [todo, setTodo] = useState({ desc: '', date: '' });
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+  }
+
+  const inputChanged = (e) => {
+    setTodo({ ...todo, [e.target.name]: e.target.value });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={addTodo}>
+        <input type="date" name="date" value={todo.date} onChange={inputChanged} />
+        <input type="text" name="desc" value={todo.desc} onChange={inputChanged} />
+        <input type="submit" value="Add" />
+      </form>
+      <Todotable todos={todos} />
     </div>
   );
 }
---
+++ src/components/todoList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function TodoList(props) {
+    return (
+        <div>
+            <table>
+                <tbody>
+                    {props.todos.map((todo, index) =>
+                        <tr key={index}>
+                            <td>{todo.desc}</td>
+                            <td>{todo.date}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
+    );
+}
